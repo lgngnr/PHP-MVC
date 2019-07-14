d56d7d55d2a92533dd3f905087036ee4e8fa6f46
Luigi <luiginigrelli@me.com>
2019-07-14 20:17:24 +0200

    Database class implemented

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,5 +1,34 @@
 <?php
 
-    
+    /**
+     * PDO Database class
+     * Connect to database
+     */
+    class Database{
+        private $host = DB_HOST;
+        private $db_name = DB_NAME;
+        private $user = DB_USER;
+        private $pass = DB_PASS;
+
+        private $dbh;
+        private $stmt;
+        private $error;
+
+        public function __construct(){
+            // Set DSN
+            $dns = "mysql://$this->host;dbname:$this->dn_name";
+            $options = array(
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            );
+            // Create PDO
+            try{
+                $this->dbh = new PDO($dns, $this->user, $this->pass);
+            }catch(PDOException $e){
+                $this->error = $e->getMessage();
+                echo $this->error;
+            }
+        }
+    }
 
 ?>
