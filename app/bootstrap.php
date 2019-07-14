f66e985a1eb1e0e04945ceca9a4b65ccf93cff25
Luigi <luiginigrelli@me.com>
2019-07-14 17:47:55 +0200

    Config file implemented

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,15 @@
 <?php 
     
+    // Load config
+    require_once 'config/config.php';
+
+    // Load libraries
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
 
     // Init Core library
     $core = new Core;
-    //print_r($core->getUrl());
 
 
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://php-mvc.com');
+    // Site Name
+    define('SITENAME', 'Custom PHP MVC framework');
+?>
