1cc171d32ee2886676d1198b219b1aa789a9b749
Luigi <luiginigrelli@me.com>
2019-07-14 20:08:28 +0200

    Add db params in config

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'php-mvc');
+    define('DB_USER', 'test');
+    define('DB_PASS', '123456');
+
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
