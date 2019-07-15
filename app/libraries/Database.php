b605ffee8a340264a250756dd83d8797801bd163
Luigi <luiginigrelli@me.com>
2019-07-15 09:22:11 +0200

    Database class cmpleted and Posts model for test

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -29,6 +29,55 @@
                 echo $this->error;
             }
         }
+
+        // prepared statement with query
+        public function query($sql){
+            $this->stmt = $this->dbh->prepare($sql);
+        }
+
+        // Bind values
+        public function bind($param, $value, $type = null){
+            if(is_null($type)){
+                switch(true){
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+                    default:
+                        $type = PDO::PARAM_STRING;
+                        break;
+                }
+            }
+
+            $this->stmt->bindValue($param, $value, $type);
+        }
+
+        // Execute prepared statement
+        public function execute(){
+            return $this->stmt->execute();
+        }
+
+        // Get result set as array of objects
+        public function resultSet(){
+            $this->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        // Get single row
+        public function single(){
+            $this->execute();
+            return $this->stmt->fetch(PDO::FETCH_OBJ);
+        }
+
+        // Row Count
+        public function rowCount(){
+            return $this->stmt->rowCount();
+        }
     }
 
 ?>
--- app/models/Post.php
+++ app/models/Post.php
@@ -5,5 +5,10 @@
         public function __construct(){
             $this->db = new Database;
         }
+
+        public function getPosts(){
+            $this->db->query("SELECT * FROM post");
+            return $this->db->resultSet();
+        }
     }
 ?>
