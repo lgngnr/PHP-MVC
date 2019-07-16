28dbc11592ead5b54159901a22db106366c550e4
Luigi <luiginigrelli@me.com>
2019-07-16 16:02:57 +0200

    Users Controller, Register method and register view implemented

---
+++ app/controllers/Users.php
@@ -0,0 +1,34 @@
+<?php
+
+    class Users extends Controller{
+
+        public function __construct(){
+
+        }
+
+        public function register(){
+            // Check for POST
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // Process form
+
+            }else{
+                // Init data
+                $data = [
+                    'name' => '',
+                    'email' => '',
+                    'password' => '',
+                    'c_password' => '',
+                    'name_error' => '',
+                    'email_error' => '',
+                    'password_error' => '',
+                    'c_password_error' => '',
+                ];
+
+                // Load the view
+                $this->view('users/register', $data);
+
+            }
+        }
+
+    }
+?>
