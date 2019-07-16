6e0bf308773564f56bf2ddfa72f7471d155f3edf
Luigi <luiginigrelli@me.com>
2019-07-16 14:43:38 +0200

    Navbar in a separate file

--- app/views/include/header.php
+++ app/views/include/header.php
@@ -10,23 +10,5 @@
     <title><?php echo SITENAME ?></title>
 </head>
 <body>
+<?php require APPROOT . "/views/include/navbar.php" ?>
 <div class="container">
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-  <a class="navbar-brand" href="<?php echo URLROOT ?>"><?php echo SITENAME; ?></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo URLROOT ?>">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo URLROOT ?>/pages/about">About</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-<!-- /Navbar -->
