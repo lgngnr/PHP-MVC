b42bb53180bf0d4e6adc09788359efc13774d5ca
Luigi <luiginigrelli@me.com>
2019-07-16 14:28:25 +0200

    Boostrap & FontAwesome configured

---
+++ tests/controllers/PagesControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    class PagesControllerTest extends TestCase{
+
+        /** 
+         * It should call index method on app/controllers/Pages.php
+         * It should load app/views/pages/index.php
+          */
+        public function testIndex(){
+
+        }
+    }
+?>
