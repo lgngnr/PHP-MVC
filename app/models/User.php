9fe074f3fe9eca9e22874ad1831ffaa5d3e342d1
Luigi <luiginigrelli@me.com>
2019-07-17 19:55:37 +0200

    getUserById - User MODEL

--- app/models/User.php
+++ app/models/User.php
@@ -69,5 +69,14 @@
                 return false;
             }
         }
+
+         // Return user by id
+        public function getUserById($id){
+            $this->db->query("SELECT * FROM users WHERE id = :id");
+            $this->db->bind(":id", $id);
+
+            return $this->db->single();
+        }
     }
+   
 ?>
