5b71f5697ce7c86afd859804315a05abde74d2f9
Luigi <luiginigrelli@me.com>
2019-07-17 15:12:31 +0200

    isLoggedIn helper

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -3,10 +3,10 @@
 
         public function __construct(){
             // If not logged redirect to login
-            if(!isset($_SESSION['user_id'])){
+            if(!isLoggedIn()){
                 header("location: " . URLROOT . "/users/login");
             }
-            
+
             $this->userModel = $this->model('Post');
         }
 
