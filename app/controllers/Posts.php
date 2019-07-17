cbe6fccf748a1fdbcc9808a814f073d140c6205b
Luigi <luiginigrelli@me.com>
2019-07-17 18:16:19 +0200

    Add method in Posts CONTROLLER

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -20,11 +20,40 @@
         }
 
         public function add(){
-            $data = [
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_error' => '',
+                    'body_error' => ''
+                ];
+
+                // Validate title
+                if(empty($data['title'])){
+                    $data['title_error'] = "Please add a title to the post";
+                }
+                // Validate body
+                if(empty($data['title'])){
+                    $data['body_error'] = "Please enter post text";
+                }
+
+                // If no error, save the post
+                if(empty($data['title_error']) && empty($data['body_error'])){
+
+                }else{
+                    $this->view('posts/add', $data);
+                }
+            }else{
+               $data = [
                 'title'=> '',
                 'body'=>''
-            ];
-            $this->view('posts/add', $data);
+                ];
+                $this->view('posts/add', $data); 
+            }
         }
     }
-?>
