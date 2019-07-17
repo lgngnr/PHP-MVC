fe1d066334ec1fca8c53bf680ffa6b9f7323553f
Luigi <luiginigrelli@me.com>
2019-07-17 19:42:54 +0200

    show VIEW implemented

---
+++ app/views/posts/show.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . "/views/include/header.php"; ?>
+
+<a href="<?php echo URLROOT?>/posts" class="btn btn-light">Back <i class="fa fa-backward"></i></a>
+
+<?php require APPROOT . "/views/include/footer.php"; ?>
