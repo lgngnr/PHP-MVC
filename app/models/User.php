731d51b9333300011e3f29c95cff559384ede695
Luigi <luiginigrelli@me.com>
2019-07-17 11:08:56 +0200

    Login method in User MODEL implemented

--- app/models/User.php
+++ app/models/User.php
@@ -35,5 +35,22 @@
                 return false;
             }
         }
+
+        // Login
+        public function login($email, $password){
+            // Getting user password from db
+            $this->db->query("SELECT * FROM users WHERE email = :email");
+            $this->db->bind(":email", $email);
+            $row = $this->db->single();
+            $hashed_password = $row->password;
+
+            // Check if password is correct
+            if(password_verify($password, $hashed_password)){
+                return $row;
+            }else{
+                return false;
+            }
+
+        }
     }
 ?>
