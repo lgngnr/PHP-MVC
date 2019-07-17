7a2579fe0059f27dae43ada211bd3634fdbdb8fe
Luigi <luiginigrelli@me.com>
2019-07-17 15:08:51 +0200

    Redirect to login if not loggedin in posts/index

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -2,6 +2,11 @@
     class Posts extends Controller{
 
         public function __construct(){
+            // If not logged redirect to login
+            if(!isset($_SESSION['user_id'])){
+                header("location: " . URLROOT . "/users/login");
+            }
+            
             $this->userModel = $this->model('Post');
         }
 
